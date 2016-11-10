e8e7663349f746466c50e99d39e6ee30c4e2ada5
James Ahlm <14782828428@qq.com>
2016-11-10 16:07:22 +0800

    Installed pug

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 var express = require('express')
 var app = express()
+var pug = require('pug');
+var multer = require('multer')
+var upload = multer({dest: 'uploads/'}) //ops go here?
+
+app.post('/upload', upload.single('f'), function(req, res, next){
+  
+  f = req.file;
+  res.send(JSON.stringify({"size": f.size}));
+
+
+}
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  //How to serve html?
+  var cf = pug.compleFile('template.pug');
+  cf({name: 'Tim'});
+  res.send(cf)
 })
 
 app.listen(3000, function () {
